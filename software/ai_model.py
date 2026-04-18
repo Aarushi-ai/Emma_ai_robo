7e6c1752efae03b9e28db566647f0d88e8b6c8f7
Aarushi Jain <btbtr24041_aarushi@banasthali.in>
2026-04-18 23:20:34 +0530

    Update ai_model.py

--- software/ai_model.py
+++ software/ai_model.py
@@ -1,12 +1,23 @@
 """
-ai_model.py
------------
-AI Brain / Language Model Integration for Emma AI Robot.
-
-Processes transcribed speech input and generates
-intelligent responses using an AI language model (e.g., GPT / Gemini / local LLM).
-
-Libraries: openai / google-generativeai / transformers
+Step2:
+text generation using gemini api
+generate api key from: https://ai.google.dev/gemini-api/docs/api-key
 """
 
-# TODO: Implement AI response generation
+import google.generativeai as genai
+
+# configure(api_key = "AIzaSyDnpqsCSYHMeF7PtDPHzeJD3skx4FfD77g")
+
+def gemini_api(text):
+    #Initialize a genAI model
+    model = genai.GenerativeModel(model_name = "gemini-2.5-flash-latest")
+    convo = model.start_chat()
+    #generate a response based on the input text.
+    response = model.generate_content(text)
+
+    print(response.text)
+
+    #-------------MAIN-------------
+
+    text = "2+2 is ?"
+    gemini_api(text)
