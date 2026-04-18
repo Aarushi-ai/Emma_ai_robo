a886423c4bf6cb981f7583b4dba60b4877f6e872
Aarushi Jain <btbtr24041_aarushi@banasthali.in>
2026-04-18 23:21:58 +0530

    Update speech_to_text_offline.py

--- software/speech_to_text_offline.py
+++ software/speech_to_text_offline.py
@@ -1,12 +1,47 @@
-"""
-speech_to_text_offline.py
---------------------------
-Offline Speech-to-Text Module for Emma AI Robot.
+'''
+step1:
+speech to text using vosk offline api
 
-Captures voice input and converts to text using a local
-offline model (e.g., Vosk or Whisper) — no internet required.
+download vosk model from: https://alphacephei.om/vosk/models
+'''
 
-Libraries: vosk / whisper, pyaudio
-"""
+import vosk
+import pyaudio
+import json
+import pygame
 
-# TODO: Implement offline speech-to-text
+pygame.mixer.init()
+model = vosk.Model(".../Resources/vosk-model-en-us-0.22")
+recognizer = vosk.KaldiRecognizer(*args:model, 16000)
+
+def play_sound(file_path):
+    pygame.mixer.music.load(file_path)
+
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():  # Wait for audio to finish playing
+        pygame.time.Clock().tick(5)
+
+
+def listen_with_vosk():
+    mic = pyaudio.PyAudio()
+    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+    stream.start_stream()
+    print("Listening...")
+    play_sound(".../Resources/listen.mp3")
+
+    while True:
+        data = stream.read(8192)
+        if len(data) == 0:
+            continue
+
+        if recognizer.AcceptWaveform(data):
+            play_sound("../Resources/convert.mp3")
+            result = recognizer.Result()
+            text = json.loads(result)["text"]
+            print("You said: "+ text)
+            return text
+        
+#------------MAIN----------------
+
+while True:
+    listen_with_vosk()
