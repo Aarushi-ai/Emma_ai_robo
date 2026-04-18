8da59ac1577ad66829f84938d7ac5e0cbbdbd098
Aarushi Jain <btbtr24041_aarushi@banasthali.in>
2026-04-18 23:24:48 +0530

    Update text_to_speech_offline.py

--- hardware/text_to_speech_offline.py
+++ hardware/text_to_speech_offline.py
@@ -1,14 +1,30 @@
 """
-text_to_speech_offline.py
---------------------------
-Offline Text-to-Speech Module for Emma AI Robot.
+step3:
+text to speech with offline library
 
-Converts AI-generated text responses into spoken audio
-using a local offline TTS engine — no internet required.
-
-Libraries: pyttsx3
 """
+import pyttsx3
 
-# TODO: Implement offline text-to-speech
-# import pyttsx3
-# engine = pyttsx3.init()
+def text_to_speech(text, voice_index=1, rate=170, volume=1.0):
+    """Convert text to speech using specified voice"""
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+
+
+    #Set properties
+    engine.setProperty(name: 'voice', voices[voice_index].id)  # 0=male, 1=female
+    engine.setProperty(name: 'rate', rate)  # speech speed
+    engine.setProperty(name: 'volume', volume)  # volume(0.0 to 1.0)
+
+
+    # speak the text
+    engine.say(text)
+    engine.runAndWait()
+
+
+#-------------MAIN----------------
+
+# Input text and voice selection
+text = "hello, my name is emma, your personal ai robot!"
+# call the function
+text_to_speech(text)
